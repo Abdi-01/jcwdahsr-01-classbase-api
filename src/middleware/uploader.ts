ce90d7b3ba3af23de4ce6283bf6228e8b2b7cf76
Abdi-01 <abdialghi@gmail.com>
2026-02-18 19:54:21 +0700

    cloudinary config

--- src/middleware/uploader.ts
+++ src/middleware/uploader.ts
@@ -2,25 +2,35 @@ import { Request } from "express";
 import multer from "multer";
 import path from "path";
 
+export const uploaderMemory = () => {
+  return multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 1 * 1024 * 1024 },
+  });
+};
+
 export const uploader = (filePrefix: string, folderDir: string) => {
-    const defaultDir = path.join(__dirname, "../public");
+  const defaultDir = path.join(__dirname, "../public");
 
-    const storage = multer.diskStorage({
-        destination: (req: Request, file: Express.Multer.File, cb: any) => {
-            const location = folderDir ? path.join(defaultDir, folderDir) : defaultDir;
+  const storage = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb: any) => {
+      const location = folderDir
+        ? path.join(defaultDir, folderDir)
+        : defaultDir;
 
-            cb(null, location);
-        },
-        filename: (req: Request, file: Express.Multer.File, cb: any) => {
-            const prefix = filePrefix || "file";
-            console.log(file.originalname.split("."));
-            const ext = file.originalname.split(".")[file.originalname.split(".").length - 1];
+      cb(null, location);
+    },
+    filename: (req: Request, file: Express.Multer.File, cb: any) => {
+      const prefix = filePrefix || "file";
+      console.log(file.originalname.split("."));
+      const ext =
+        file.originalname.split(".")[file.originalname.split(".").length - 1];
 
-            const newName = `${prefix}_${Date.now()}.${ext}`;
+      const newName = `${prefix}_${Date.now()}.${ext}`;
 
-            cb(null, newName);
-        }
-    })
+      cb(null, newName);
+    },
+  });
 
-    return multer({ storage })
-}
+  return multer({ storage });
+};
